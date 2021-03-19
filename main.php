e69c31a54e617cf252b0361f5e8b7866188563dd
yuriksensej <yuraminchenko@gmail.com>
2021-03-19 23:31:09 +0200

    19.03.21

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+// echo 'hello'; //
+// echo 'hello'; //
+$test = '1' + '23';
+echo $test . PHP_EOL;
+var_dump($test) . PHP_EOL;
+$arr = [1, 2, 3];
+$obj = (object) $arr;
+$obj->name = 2;
+$obj->{2} = 22;
+// var_dump($obj);
